ae5c7b2862f7a880fa21a7388fe7f2800103759e
Mayank Ghanshala <mayankghanshala@gmail.com>
2025-09-29 18:02:28 +0530

    Waypoint custom editor

--- Assets/DeadEarth/Editor/AIWayPointNetworkEditor.cs
+++ Assets/DeadEarth/Editor/AIWayPointNetworkEditor.cs
@@ -1,18 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class AIWayPointNetworkEditor : MonoBehaviour
+[CustomEditor(typeof(AIWayPointNetwork))]
+public class AIWayPointNetworkEditor : Editor
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnInspectorGUI()
     {
-        
+        AIWayPointNetwork network = (AIWayPointNetwork)target;
+        network.DisplayMode = (DisplayMode)EditorGUILayout.EnumPopup("Display Mode",network.DisplayMode);
+        network.UIStart = (int)EditorGUILayout.IntSlider("UIStart", network.UIStart, 0,network.WayPoints.Count-1);
+        network.UIEnd = (int)EditorGUILayout.IntSlider("UIStart", network.UIEnd, 0, network.WayPoints.Count - 1);
+        DrawDefaultInspector();
+    }
+    private void OnSceneGUI()
+    {
+        AIWayPointNetwork network = (AIWayPointNetwork)target;
+        for (int i = 0; i < network.WayPoints.Count; ++i)
+        {
+            if (network.WayPoints[i] != null)
+            {
+                Handles.Label(network.WayPoints[i].position,$"WayPoint {i+1}");
+            }
+        }
+
+        if (network.DisplayMode == DisplayMode.Connections)
+        {
+            Vector3[] linePositions = new Vector3[network.WayPoints.Count+1];
+            for (int i = 0; i <= network.WayPoints.Count; ++i)
+            {
+                int index = i != network.WayPoints.Count ? i : 0;
+                linePositions[i] = network.WayPoints[index] != null ? network.WayPoints[index].position : new Vector3(Mathf.Infinity,Mathf.Infinity);
+            }
+            Handles.color = Color.cyan;
+            Handles.DrawPolyLine(linePositions);
+        }
+        else if(network.DisplayMode == DisplayMode.Paths)
+        {
+            if (network.WayPoints[network.UIStart] != null && network.WayPoints[network.UIEnd] != null)
+            {
+                NavMeshPath path = new NavMeshPath();
+                Vector3 from = network.WayPoints[network.UIStart].position;
+                Vector3 to = network.WayPoints[network.UIEnd].position;
+                NavMesh.CalculatePath(from, to, NavMesh.AllAreas, path);
+                Handles.color = Color.yellow;
+                Handles.DrawPolyLine(path.corners);
+            }
+        }
     }
 }
--- Assets/DeadEarth/Scripts/AIWayPointNetwork.cs
+++ Assets/DeadEarth/Scripts/AIWayPointNetwork.cs
@@ -1,7 +1,18 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-
+public enum DisplayMode
+{
+    None,
+    Connections,
+    Paths
+}
 public class AIWayPointNetwork : MonoBehaviour
 {
+    [HideInInspector]
+    public DisplayMode DisplayMode = DisplayMode.Connections;
+    [HideInInspector]
+    public int UIStart = 0;
+    [HideInInspector]
+    public int UIEnd = 0;
     public List<Transform> WayPoints;
 }

b2edd376b590e5dfa434a808dc110fe8b306f29f
Mayank Ghanshala <mayankghanshala@gmail.com>
2025-09-30 15:16:28 +0530

    NavMeshAgentMovement

--- Assets/DeadEarth/Editor/AIWayPointNetworkEditor.cs
+++ Assets/DeadEarth/Editor/AIWayPointNetworkEditor.cs
@@ -9,8 +9,8 @@ public class AIWayPointNetworkEditor : Editor
     public override void OnInspectorGUI()
     {
         AIWayPointNetwork network = (AIWayPointNetwork)target;
-        network.DisplayMode = (DisplayMode)EditorGUILayout.EnumPopup("Display Mode",network.DisplayMode);
-        network.UIStart = (int)EditorGUILayout.IntSlider("UIStart", network.UIStart, 0,network.WayPoints.Count-1);
+        network.DisplayMode = (DisplayMode)EditorGUILayout.EnumPopup("Display Mode", network.DisplayMode);
+        network.UIStart = (int)EditorGUILayout.IntSlider("UIStart", network.UIStart, 0, network.WayPoints.Count - 1);
         network.UIEnd = (int)EditorGUILayout.IntSlider("UIStart", network.UIEnd, 0, network.WayPoints.Count - 1);
         DrawDefaultInspector();
     }
@@ -21,22 +21,22 @@ public class AIWayPointNetworkEditor : Editor
         {
             if (network.WayPoints[i] != null)
             {
-                Handles.Label(network.WayPoints[i].position,$"WayPoint {i+1}");
+                Handles.Label(network.WayPoints[i].position, $"WayPoint {i + 1}");
             }
         }
 
         if (network.DisplayMode == DisplayMode.Connections)
         {
-            Vector3[] linePositions = new Vector3[network.WayPoints.Count+1];
+            Vector3[] linePositions = new Vector3[network.WayPoints.Count + 1];
             for (int i = 0; i <= network.WayPoints.Count; ++i)
             {
                 int index = i != network.WayPoints.Count ? i : 0;
-                linePositions[i] = network.WayPoints[index] != null ? network.WayPoints[index].position : new Vector3(Mathf.Infinity,Mathf.Infinity);
+                linePositions[i] = network.WayPoints[index] != null ? network.WayPoints[index].position : new Vector3(Mathf.Infinity, Mathf.Infinity);
             }
             Handles.color = Color.cyan;
             Handles.DrawPolyLine(linePositions);
         }
-        else if(network.DisplayMode == DisplayMode.Paths)
+        else if (network.DisplayMode == DisplayMode.Paths)
         {
             if (network.WayPoints[network.UIStart] != null && network.WayPoints[network.UIEnd] != null)
             {
---
+++ Assets/Navigation Example/Scripts/NavMeshAgentExample.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentExample : MonoBehaviour
+{
+    [SerializeField] private AIWayPointNetwork _aIWayPointNetwork;
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+    [SerializeField] private int _currentIndex;
+    [SerializeField] private bool _hasPath;
+    [SerializeField] private bool _pathPending;
+    [SerializeField] private bool _isPathStale;
+    [SerializeField] private NavMeshPathStatus _pathStatus;
+    private void Start()
+    {
+        //_navMeshAgent.updatePosition = false;
+        //_navMeshAgent.updateRotation = false;
+        SetDestination(false);
+    }
+
+    void SetDestination(bool shouldIncrement)
+    {
+        if (_aIWayPointNetwork == null || _aIWayPointNetwork.WayPoints == null)
+            return;
+        int incStep = shouldIncrement ? 1 : 0;
+        int targetIndex = _currentIndex + incStep >= _aIWayPointNetwork.WayPoints.Count ? 0 : _currentIndex + incStep;
+        Transform targetTransform = _aIWayPointNetwork.WayPoints[targetIndex];
+        if (targetTransform != null)
+        {
+            _navMeshAgent.destination = targetTransform.position;
+            _currentIndex = targetIndex;
+            return;
+        }
+        _currentIndex++;
+
+    }
+
+    private void Update()
+    {
+        _hasPath = _navMeshAgent.hasPath;
+        _pathPending = _navMeshAgent.pathPending;
+        _isPathStale = _navMeshAgent.isPathStale;
+        _pathStatus = _navMeshAgent.pathStatus;
+
+        if ((!_hasPath && !_pathPending) || _pathStatus == NavMeshPathStatus.PathInvalid)
+        {
+            SetDestination(true);
+        }
+        else if (_isPathStale)
+            SetDestination(false);
+    }
+}
